e495c34b3ba3654a7d3023c9fa1d30020cb92997
fiixed <fiixed@gmail.com>
2021-02-03 17:38:52 -0500

    links working

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -6,6 +6,7 @@ import {
     UserOutlined,
     UserAddOutlined 
 } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 const { SubMenu, Item } = Menu;
 
@@ -20,13 +21,13 @@ const Header = () => {
     return (
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
             <Item key="home" icon={<AppstoreOutlined />}>
-            Home
+                <Link to='/'>Home</Link>
             </Item>
             <Item key="register" icon={<UserAddOutlined />} className='float-right'>
-            Register
+            <Link to='/login'>Login</Link>
             </Item>
             <Item key="login" icon={<UserOutlined />} className='float-right'>
-            Login
+            <Link to='/register'>Register</Link>
             </Item>
             <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
                 <Item key="setting:1">Option 1</Item>
