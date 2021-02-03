e5d0e4c257c3da424472818d7c6046b665815d09
fiixed <fiixed@gmail.com>
2021-02-03 15:54:34 -0500

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { 
+  Switch, 
+  Route 
+} from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/Home'
 
-const App = () => (
-    <div>
-      <p>React App</p>
-    </div>
-);
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/login' component={Login}/>
+      <Route path='/register' component={Register}/>
+    </Switch>
+  )
+}
 
 export default App;
