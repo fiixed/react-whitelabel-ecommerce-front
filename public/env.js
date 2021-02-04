915c4f6fe35d426a8098d1ad7dd12a8da85e66eb
fiixed <fiixed@gmail.com>
2021-02-03 20:07:22 -0500

    signinlinktoemail working

--- public/env.js
+++ public/env.js
@@ -1,1 +1,9 @@
-window.env = {};
+window.env = {
+  "API_KEY": "AIzaSyAvktCKCwYXmzTxZIpImxpJydpXouGRwME",
+  "AUTH_DOMAIN": "react-whitelabel-ecommerce.firebaseapp.com",
+  "PROJECT_ID": "react-whitelabel-ecommerce",
+  "STORAGE_BUCKET": "react-whitelabel-ecommerce.appspot.com",
+  "MESSAGE_SENDER_ID": "1000576983000",
+  "APP_ID": "1:1000576983000:web:24afec1036c20281ff9cad",
+  "MEASUREMENT_ID": "G-S32W1XWFE6"
+};
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react'
+import { auth } from '../../firebase'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Register = () => {
     const [email, setEmail] = useState('');
-    const handleSubmit = () => {
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const config = {
+            url: 'http://localhost:3000/register/complete',
+            handleCodeInApp: true
+        }
 
+        await auth.sendSignInLinkToEmail(email, config);
+        toast.success(`Confirmation sent to ${email}.  Click the link to complete registration`);
+
+        // save user email in local storage
+        window.localStorage.setItem('emailForRegistration', email);
+
+        // clear state
+        setEmail('');
     }
+
     const registerForm = () => <form onSubmit={handleSubmit}>
         <input 
         type='email' 
@@ -21,6 +39,7 @@ const Register = () => {
             <div className='row'>
                 <div className='col-md-6 offset-md-3'>
                     <h4>Register</h4>
+                    <ToastContainer />
                     {registerForm()}
                 </div>
             </div>
