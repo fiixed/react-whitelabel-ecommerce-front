0cf77dcdf15a0020e00331fb5d72b0e22e7783af
fiixed <fiixed@gmail.com>
2021-02-03 20:34:09 -0500

    env for url redirect and refactoring of toastcontainer

--- public/env.js
+++ public/env.js
@@ -5,5 +5,6 @@ window.env = {
   "STORAGE_BUCKET": "react-whitelabel-ecommerce.appspot.com",
   "MESSAGE_SENDER_ID": "1000576983000",
   "APP_ID": "1:1000576983000:web:24afec1036c20281ff9cad",
-  "MEASUREMENT_ID": "G-S32W1XWFE6"
+  "MEASUREMENT_ID": "G-S32W1XWFE6",
+  "REGISTER_REDIRECT_URL": "http://localhost:3000/register/complete"
 };
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+import env from "react-dotenv";
 import { auth } from '../../firebase'
-import { toast, ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { toast } from 'react-toastify'
+
+
 
 const Register = () => {
     const [email, setEmail] = useState('');
@@ -9,7 +11,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const config = {
-            url: 'http://localhost:3000/register/complete',
+            url: env.REGISTER_REDIRECT_URL,
             handleCodeInApp: true
         }
 
@@ -39,7 +41,7 @@ const Register = () => {
             <div className='row'>
                 <div className='col-md-6 offset-md-3'>
                     <h4>Register</h4>
-                    <ToastContainer />
+                    
                     {registerForm()}
                 </div>
             </div>
