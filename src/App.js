a4b909e0235c98e83c6d78c943289fbdba51e864
fiixed <fiixed@gmail.com>
2021-02-05 17:13:06 -0500

    forgot password working

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Home from './pages/Home'
 
 import { auth } from './firebase'
 import { useDispatch } from 'react-redux'
+import ForgotPassword from './pages/auth/ForgotPassword'
 
 
 const App = () => {
@@ -48,7 +49,9 @@ const App = () => {
         <Route exact path='/login' component={Login}/>
         <Route exact path='/register' component={Register}/>
         <Route exact path='/register/complete' component={RegisterComplete}/>
+        <Route exact path='/forgot/password' component={ForgotPassword}/>
       </Switch>
+      
     </>
   )
 }
