4655a4b0edaf2caf751888bca55a0721c55928cb
fiixed <fiixed@gmail.com>
2021-02-05 11:46:25 -0500

    conditional menu items

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -9,7 +9,7 @@ import {
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import firebase from 'firebase';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 const { SubMenu, Item } = Menu;
@@ -17,6 +17,7 @@ const { SubMenu, Item } = Menu;
 const Header = () => {
     const [current, setCurrent] = useState('home');
     let dispatch = useDispatch();
+    let { user } = useSelector((state) => ({...state}));
     let history = useHistory();
 
     const handleClick = e => {
@@ -39,17 +40,19 @@ const Header = () => {
             <Item key="home" icon={<AppstoreOutlined />}>
                 <Link to='/'>Home</Link>
             </Item>
-            <Item key="register" icon={<UserAddOutlined />} className='float-right'>
+            {!user && (<Item key="register" icon={<UserAddOutlined />} className='float-right'>
             <Link to='/login'>Login</Link>
-            </Item>
-            <Item key="login" icon={<UserOutlined />} className='float-right'>
+            </Item>)}
+            {!user && (<Item key="login" icon={<UserOutlined />} className='float-right'>
             <Link to='/register'>Register</Link>
-            </Item>
-            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+            </Item>)}
+            {
+                user && (<SubMenu key="SubMenu" icon={<SettingOutlined />} title={user.email && user.email.split('@')[0]} className="float-right">
                 <Item key="setting:1">Option 1</Item>
                 <Item key="setting:2">Option 2</Item>
                 <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
-            </SubMenu>
+            </SubMenu>)
+            }
       </Menu>
     )
 }
