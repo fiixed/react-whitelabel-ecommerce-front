7905d9e4cae3a655804affd5700e0917903d6b20
fiixed <fiixed@gmail.com>
2021-02-06 17:44:07 -0500

    get current user from db

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -45,7 +45,7 @@ const Login = ({history}) => {
                     },
                   });
             })
-            .catch()
+            .catch(err => console.log(err))
             
             history.push('/');
         } catch (error) {
@@ -73,7 +73,7 @@ const Login = ({history}) => {
                     },
                   });
             })
-            .catch()
+            .catch(err => console.log(err))
             history.push('/');
         })
         .catch(err => {
