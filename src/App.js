c9c5d397def92dc2cc36cadba5a7312c35091d7a
fiixed <fiixed@gmail.com>
2021-02-10 15:55:16 -0500

    subcreate

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ import { useDispatch } from 'react-redux'
 import { currentUser } from './functions/auth'
 import CategoryCreate from './pages/admin/category/CategoryCreate'
 import CategoryUpdate from './pages/admin/category/CategoryUpdate'
+import SubCreate from './pages/admin/sub/SubCreate'
 
 const App = () => {
   const dispatch = useDispatch();
@@ -73,6 +74,7 @@ const App = () => {
         <AdminRoute exact path='/admin/dashboard' component={AdminDashboard}/>
         <AdminRoute exact path='/admin/category' component={CategoryCreate}/>
         <AdminRoute exact path='/admin/category/:slug' component={CategoryUpdate}/>
+        <AdminRoute exact path='/admin/sub' component={SubCreate}/>
       </Switch>
       
     </>
