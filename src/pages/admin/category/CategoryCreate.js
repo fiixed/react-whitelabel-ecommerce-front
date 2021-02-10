67b734ca07c66c41bc6e9a75be77ffb9cd3b2291
fiixed <fiixed@gmail.com>
2021-02-10 15:21:39 -0500

    local search component

--- src/pages/admin/category/CategoryCreate.js
+++ src/pages/admin/category/CategoryCreate.js
@@ -10,6 +10,7 @@ import {
 import { Link } from "react-router-dom";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import CategoryForm from '../../../components/forms/CategoryForm';
+import LocalSearch from "../../../components/forms/LocalSearch";
 
 const CategoryCreate = () => {
   const { user } = useSelector((state) => ({ ...state }));
@@ -66,10 +67,7 @@ const CategoryCreate = () => {
     }
   };
 
-  const handleSearchChange = (e) => {
-    e.preventDefault()
-    setKeyword(e.target.value.toLowerCase());
-  }
+  
 
   const searched = (keyword) => (c) => c.name.toLowerCase().includes(keyword);
 
@@ -86,8 +84,8 @@ const CategoryCreate = () => {
             <h4>Create category</h4>
           )}
           <CategoryForm handleSubmit={handleSubmit} name={name} setName={setName} />
-          <input type='search' placeholder='Filter' value={keyword} onChange={handleSearchChange} className='form-control mb-4' />
-          <hr />
+          <LocalSearch keyword={keyword} setKeyword={setKeyword}/>
+         
           {categories.filter(searched(keyword)).map((c) => (
             <div className="alert alert-secondary" key={c._id}>
               {c.name}
