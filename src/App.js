3eeb7d3fa6b26cc7d9e4371d32f86b371f67f182
fiixed <fiixed@gmail.com>
2021-02-11 17:43:31 -0500

    create product page

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import CategoryCreate from './pages/admin/category/CategoryCreate'
 import CategoryUpdate from './pages/admin/category/CategoryUpdate'
 import SubCreate from './pages/admin/sub/SubCreate'
 import SubUpdate from './pages/admin/sub/SubUpdate'
+import ProductCreate from './pages/admin/product/ProductCreate'
 
 const App = () => {
   const dispatch = useDispatch();
@@ -77,6 +78,8 @@ const App = () => {
         <AdminRoute exact path='/admin/category/:slug' component={CategoryUpdate}/>
         <AdminRoute exact path='/admin/sub' component={SubCreate}/>
         <AdminRoute exact path='/admin/sub/:slug' component={SubUpdate}/>
+        <AdminRoute exact path='/admin/product' component={ProductCreate}/>
+        
         
       </Switch>
       
