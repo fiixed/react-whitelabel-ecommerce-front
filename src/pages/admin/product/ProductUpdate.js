1a03c34d976e23dbb644b80a33775333267bc531
fiixed <fiixed@gmail.com>
2021-02-15 12:21:24 -0500

    pre populate subs

--- src/pages/admin/product/ProductUpdate.js
+++ src/pages/admin/product/ProductUpdate.js
@@ -27,6 +27,7 @@ const ProductUpdate = ({ match }) => {
     const [values, setValues] = useState(initialState);
     const [categories, setCategories] = useState([]);
     const [subOptions, setSubOptions] = useState([]);
+    const [arrayOfSubs, setArrayOfSubs] = useState([]);
   
     const { user } = useSelector((state) => ({ ...state }));
     // router
@@ -39,11 +40,24 @@ const ProductUpdate = ({ match }) => {
     }, []);
   
     const loadProduct = () => {
-      getProduct(slug).then((p) => {
-        // console.log("single product", p);
-        setValues({ ...values, ...p.data });
-      });
-    };
+        getProduct(slug).then((p) => {
+          // console.log("single product", p);
+          // 1 load single proudct
+          setValues({ ...values, ...p.data });
+          // 2 load single product category subs
+          getCategorySubs(p.data.category._id).then((res) => {
+            setSubOptions(res.data); // on first load, show default subs
+            console.log('DEFAULT SUBS', res.data);
+          });
+          // 3 prepare array of sub ids to show as default sub values in antd Select
+          let arr = [];
+          p.data.subs.map((s) => {
+            arr.push(s._id);
+          });
+          console.log("ARR", arr);
+          setArrayOfSubs((prev) => arr); // required for ant design select to work.  Required for state, prev value is replace with new value
+        });
+      };
 
     const loadCategories = () =>
     getCategories().then((c) => {
@@ -90,6 +104,8 @@ const ProductUpdate = ({ match }) => {
                 handleCatagoryChange={handleCategoryChange}
                 categories={categories}
                 subOptions={subOptions}
+                arrayOfSubs={arrayOfSubs}
+                setArrayOfSubs={setArrayOfSubs}
             />
             <hr />
           </div>
