3ca20bc847165fec1beb95d1a0e6f06744504921
fiixed <fiixed@gmail.com>
2021-02-15 11:38:26 -0500

    prepopulate color and brand on product update form

--- src/pages/admin/product/ProductUpdate.js
+++ src/pages/admin/product/ProductUpdate.js
@@ -2,32 +2,80 @@ import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { createProduct } from "../../../functions/product";
-import ProductCreateForm from "../../../components/forms/ProductCreateForm";
+import { getProduct } from "../../../functions/product";
 import { getCategories, getCategorySubs } from "../../../functions/category";
 import FileUpload from "../../../components/forms/FileUpload";
 import { LoadingOutlined } from "@ant-design/icons";
+import ProductUpdateForm from "../../../components/forms/ProductUpdateForm";
+
+const initialState = {
+  title: "",
+  description: "",
+  price: "",
+  categories: [],
+  category: "",
+  subs: [],
+  shipping: "",
+  quantity: "",
+  images: [],
+  colors: ['Black', 'Brown', 'Silver', 'White', 'Blue', 'Red', 'Gold'],
+  brands: ['Apple', 'Samsung', 'Microsoft', 'Lenovo', 'Asus', 'MSI', 'Google', 'HP', 'Dell', 'Acer'],
+  color: "",
+  brand: "",
+};
 
 const ProductUpdate = ({ match }) => {
-  const { user } = useSelector((state) => ({ ...state }));
-  // router
-  const { slug } = match.params;
-
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-2">
-          <AdminNav />
-        </div>
-
-        <div className="col-md-10">
-          <h4>Product update</h4>
-          {JSON.stringify(slug)}
-          <hr />
+    // state
+    const [values, setValues] = useState(initialState);
+  
+    const { user } = useSelector((state) => ({ ...state }));
+    // router
+    const { slug } = match.params;
+  
+    useEffect(() => {
+      loadProduct();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+  
+    const loadProduct = () => {
+      getProduct(slug).then((p) => {
+        // console.log("single product", p);
+        setValues({ ...values, ...p.data });
+      });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      //
+    };
+  
+    const handleChange = (e) => {
+      setValues({ ...values, [e.target.name]: e.target.value });
+      // console.log(e.target.name, " ----- ", e.target.value);
+    };
+  
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-2">
+            <AdminNav />
+          </div>
+  
+          <div className="col-md-10">
+            <h4>Product update</h4>
+             {JSON.stringify(values)} 
+  
+            <ProductUpdateForm
+              handleSubmit={handleSubmit}
+              handleChange={handleChange}
+              setValues={setValues}
+              values={values}
+            />
+            <hr />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default ProductUpdate;
