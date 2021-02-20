4e9db55775ee44bae4ad5e1f73421b9fdacaa3f7
fiixed <fiixed@gmail.com>
2021-02-20 12:31:40 -0500

    search component

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -11,6 +11,7 @@ import { Link } from 'react-router-dom';
 import firebase from 'firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Search from "../forms/Search";
 
 const { SubMenu, Item } = Menu;
 
@@ -37,35 +38,51 @@ const Header = () => {
 
     return (
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-            <Item key="home" icon={<AppstoreOutlined />}>
-                <Link to='/'>Home</Link>
+          <Item key="home" icon={<AppstoreOutlined />}>
+            <Link to="/">Home</Link>
+          </Item>
+    
+          {!user && (
+            <Item key="register" icon={<UserAddOutlined />} className="float-right">
+              <Link to="/register">Register</Link>
             </Item>
-            {!user && (<Item key="register" icon={<UserAddOutlined />} className='float-right'>
-            <Link to='/login'>Login</Link>
-            </Item>)}
-            {!user && (<Item key="login" icon={<UserOutlined />} className='float-right'>
-            <Link to='/register'>Register</Link>
-            </Item>)}
-            {
-                user && (<SubMenu key="SubMenu" icon={<SettingOutlined />} title={user.email && user.email.split('@')[0]} className="float-right">
-
-                {user && user.role === 'subscriber' && (
-                    <Item>
-                        <Link to='/user/history'>Dashboard</Link>
-                    </Item>
-                )}
-
-                {user && user.role === 'admin' && (
-                    <Item>
-                        <Link to='/admin/dashboard'>Dashboard</Link>
-                    </Item>
-                )}
-
-                <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
-            </SubMenu>)
-            }
-      </Menu>
-    )
-}
+          )}
+    
+          {!user && (
+            <Item key="login" icon={<UserOutlined />} className="float-right">
+              <Link to="/login">Login</Link>
+            </Item>
+          )}
+    
+          {user && (
+            <SubMenu
+              icon={<SettingOutlined />}
+              title={user.email && user.email.split("@")[0]}
+              className="float-right"
+            >
+              {user && user.role === "subscriber" && (
+                <Item>
+                  <Link to="/user/history">Dashboard</Link>
+                </Item>
+              )}
+    
+              {user && user.role === "admin" && (
+                <Item>
+                  <Link to="/admin/dashboard">Dashboard</Link>
+                </Item>
+              )}
+    
+              <Item icon={<LogoutOutlined />} onClick={logout}>
+                Logout
+              </Item>
+            </SubMenu>
+          )}
+    
+          <span className="float-right p-1">
+            <Search />
+          </span>
+        </Menu>
+      );
+    };
 
 export default Header
