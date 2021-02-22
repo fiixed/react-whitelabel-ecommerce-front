64e62ab7fce6b5c7ea5b7bd6c16cdb1e5ce99879
fiixed <fiixed@gmail.com>
2021-02-22 17:13:50 -0500

    filter by category

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -3,10 +3,11 @@ import {
   getProductsByCount,
   fetchProductsByFilter,
 } from "../functions/product";
+import { getCategories } from "../functions/category";
 import { useSelector, useDispatch } from "react-redux";
 import ProductCard from "../components/cards/ProductCard";
-import { Menu, Slider } from "antd";
-import { DollarOutlined } from "@ant-design/icons";
+import { Menu, Slider, Checkbox } from "antd";
+import { DollarOutlined, DownSquareOutlined } from "@ant-design/icons";
 
 const { SubMenu, ItemGroup } = Menu;
 
@@ -15,6 +16,8 @@ const Shop = () => {
   const [loading, setLoading] = useState(false);
   const [price, setPrice] = useState([0, 0]);
   const [ok, setOk] = useState(false);
+  const [categories, setCategories] = useState([]);  // populates the side menu
+  const [categoryIds, setCategoryIds] = useState([]);  // used to be sent to the back end to peform the search query
 
   let dispatch = useDispatch();
   let { search } = useSelector((state) => ({ ...state }));
@@ -22,6 +25,8 @@ const Shop = () => {
 
   useEffect(() => {
     loadAllProducts();
+    // fetch categories
+    getCategories().then((res) => setCategories(res.data));
   }, []);
 
   const fetchProducts = (arg) => {
@@ -63,6 +68,51 @@ const Shop = () => {
     }, 300);
   };
 
+  // 4. load products based on category
+  // show categories in a list of checkbox
+  const showCategories = () =>
+    categories.map((c) => (
+      <div key={c._id}>
+        <Checkbox
+          onChange={handleCheck}
+          className="pb-2 pl-4 pr-4"
+          value={c._id}
+          name="category"
+          checked={categoryIds.includes(c._id)}  // check if the category in the state includes the given category id - true/false
+        >
+          {c.name}
+        </Checkbox>
+        <br />
+      </div>
+    ));
+
+    // handle check for categories
+  const handleCheck = (e) => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: "" },
+    });
+    setPrice([0, 0]);
+    // console.log(e.target.value);
+
+    // next few lines ensures we don't send duplicate categoryId's to the backend
+    let inTheState = [...categoryIds];
+    let justChecked = e.target.value;
+    let foundInTheState = inTheState.indexOf(justChecked); // index or -1
+
+    // indexOf method ?? if not found returns -1 else return index [1,2,3,4,5]
+    if (foundInTheState === -1) {
+      inTheState.push(justChecked);
+    } else {
+      // if found pull out one item from index
+      inTheState.splice(foundInTheState, 1);
+    }
+
+    setCategoryIds(inTheState);
+    // console.log(inTheState);
+    fetchProducts({ category: inTheState });  // make the call without the duplicates
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -70,7 +120,8 @@ const Shop = () => {
           <h4>Search/Filter</h4>
           <hr />
 
-          <Menu defaultOpenKeys={["1", "2"]} mode="inline">  
+          <Menu defaultOpenKeys={["1", "2"]} mode="inline">
+            {/* price */}
             <SubMenu
               key="1"
               title={
@@ -82,13 +133,25 @@ const Shop = () => {
               <div>
                 <Slider
                   className="ml-4 mr-4"
-                  tipFormatter={(v) => `$${v}`} // shows what the dollar value is as slider moves
+                  tipFormatter={(v) => `$${v}`}
                   range
                   value={price}
                   onChange={handleSlider}
                   max="4999"
                 />
               </div>
+            </SubMenu>
+
+            {/* category */}
+            <SubMenu
+              key="2"
+              title={
+                <span className="h6">
+                  <DownSquareOutlined /> Categories
+                </span>
+              }
+            >
+              <div style={{ maringTop: "-10px" }}>{showCategories()}</div>
             </SubMenu>
           </Menu>
         </div>
