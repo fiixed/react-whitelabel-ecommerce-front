ee4e070ddf7b8ad19339f623ee2640dc06ae80e2
fiixed <fiixed@gmail.com>
2021-02-23 17:31:27 -0500

    cart page scaffolding

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ import Product from "./pages/Product";
 import CategoryHome from "./pages/category/CategoryHome";
 import SubHome from "./pages/sub/SubHome";
 import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -95,6 +96,7 @@ const App = () => {
         <Route exact path="/category/:slug" component={CategoryHome} />
         <Route exact path="/sub/:slug" component={SubHome} />
         <Route exact path="/shop" component={Shop} />
+        <Route exact path="/cart" component={Cart} />
         
         
       </Switch>
