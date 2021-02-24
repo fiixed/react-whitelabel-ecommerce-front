9fbc7f0a153976063a3930a44f3e1c43f362f107
fiixed <fiixed@gmail.com>
2021-02-24 12:21:27 -0500

    update count in cart

--- src/components/cards/ProductCardInCheckout.js
+++ src/components/cards/ProductCardInCheckout.js
@@ -2,9 +2,10 @@ import React from "react";
 import ModalImage from "react-modal-image";
 import laptop from "../../images/laptop.png";
 import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 
 const ProductCardInCheckout = ({ p }) => {
-  const colors = ["Black", "Brown", "Silver", "White", "Blue"];
+  const colors = ['Black', 'Brown', 'Silver', 'White', 'Blue', 'Red', 'Gold'];
   let dispatch = useDispatch();
 
   const handleColorChange = (e) => {
@@ -18,11 +19,41 @@ const ProductCardInCheckout = ({ p }) => {
 
       cart.map((product, i) => {
         if (product._id === p._id) {
-          cart[i].color = e.target.value;
+           cart[i].color = e.target.value;
         }
       });
 
-      //  console.log('cart udpate color', cart)
+        console.log('cart udpate color', cart)
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  };
+
+  const handleQuantityChange = (e) => {
+    // console.log("available quantity", p.quantity);
+    let count = e.target.value < 1 ? 1 : e.target.value;
+
+    if (count > p.quantity) {
+      toast.error(`Max available quantity: ${p.quantity}`);
+      return;
+    }
+
+    let cart = [];
+
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+
+      cart.map((product, i) => {
+        if (product._id == p._id) {
+          cart[i].count = count;
+        }
+      });
+
       localStorage.setItem("cart", JSON.stringify(cart));
       dispatch({
         type: "ADD_TO_CART",
@@ -52,13 +83,13 @@ const ProductCardInCheckout = ({ p }) => {
             name="color"
             className="form-control"
           >
-            {p.color ? (
+            {p.color ? ( // if the product has an existing color
               <option value={p.color}>{p.color}</option>
             ) : (
               <option>Select</option>
             )}
             {colors
-              .filter((c) => c !== p.color)
+              .filter((c) => c !== p.color) // exclude existing color so it doesnt repeat in the colors list
               .map((c) => (
                 <option key={c} value={c}>
                   {c}
@@ -66,7 +97,14 @@ const ProductCardInCheckout = ({ p }) => {
               ))}
           </select>
         </td>
-        <td>{p.count}</td>
+        <td className="text-center">
+          <input
+            type="number"
+            className="form-control"
+            value={p.count}
+            onChange={handleQuantityChange}
+          />
+        </td>
         <td>Shipping Icon</td>
         <td>Delete Icon</td>
       </tr>
