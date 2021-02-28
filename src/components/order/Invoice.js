ba22aec07622191716f406c32bfaf65f3da0f929
fiixed <fiixed@gmail.com>
2021-02-28 18:13:46 -0500

    fix invoice

--- src/components/order/Invoice.js
+++ src/components/order/Invoice.js
@@ -15,7 +15,7 @@ const Invoice = ({ order }) => (
         ~ {new Date().toLocaleString()} ~
       </Text>
       <Text style={styles.title}>Order Invoice</Text>
-      <Text style={styles.author}>React Redux Ecommerce</Text>
+      <Text style={styles.author}>fiixed Ecommerce</Text>
       <Text style={styles.subtitle}>Order Summary</Text>
 
       <Table>
